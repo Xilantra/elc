812770d87415f7eab46963e731afa94b650ff7bf
Afiq Xilantra Azmi <artslavehuh@yahoo.com>
2014-07-23 05:46:46 +0800

    Bongok punya calendar

--- js/app.js
+++ js/app.js
@@ -68,8 +68,9 @@ function CalendarCtrl($scope) {
             className: 'gcal-event',           // an option!
             currentTimezone: 'America/Chicago' // an option!
     };
- 
-     $scope.events = [
+/*
+    /* event source that contains custom events on the scope  
+    $scope.events = [
       {title: 'All Day Event',start: new Date(y, m, 1)},
       {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
       {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
@@ -77,7 +78,8 @@ function CalendarCtrl($scope) {
       {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
       {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
     ];
-     $scope.eventsF = function (start, end, callback) {
+    /* event source that calls a function on every view switch  
+    $scope.eventsF = function (start, end, callback) {
       var s = new Date(start).getTime() / 1000;
       var e = new Date(end).getTime() / 1000;
       var m = new Date(start).getMonth();
@@ -94,16 +96,20 @@ function CalendarCtrl($scope) {
           {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
         ]
     };
-     $scope.alertOnEventClick = function( event, allDay, jsEvent, view ){
+    /* alert on eventClick  
+    $scope.alertOnEventClick = function( event, allDay, jsEvent, view ){
         $scope.alertMessage = (event.title + ' was clicked ');
     };
-      $scope.alertOnDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){
+    /* alert on Drop  
+     $scope.alertOnDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){
        $scope.alertMessage = ('Event Droped to make dayDelta ' + dayDelta);
     };
-     $scope.alertOnResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ){
+    /* alert on Resize  
+    $scope.alertOnResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ){
        $scope.alertMessage = ('Event Resized to make dayDelta ' + minuteDelta);
     };
-     $scope.addRemoveEventSource = function(sources,source) {
+    /* add and removes an event source of choice  
+    $scope.addRemoveEventSource = function(sources,source) {
       var canAdd = 0;
       angular.forEach(sources,function(value, key){
         if(sources[key] === source){
@@ -115,7 +121,8 @@ function CalendarCtrl($scope) {
         sources.push(source);
       }
     };
-     $scope.addEvent = function() {
+    /* add custom event 
+    $scope.addEvent = function() {
       $scope.events.push({
         title: 'Open Sesame',
         start: new Date(y, m, 28),
@@ -123,16 +130,20 @@ function CalendarCtrl($scope) {
         className: ['openSesame']
       });
     };
-     $scope.remove = function(index) {
+    /* remove event  
+    $scope.remove = function(index) {
       $scope.events.splice(index,1);
     };
-     $scope.changeView = function(view,calendar) {
+    /* Change View  
+    $scope.changeView = function(view,calendar) {
       calendar.fullCalendar('changeView',view);
     };
-     $scope.renderCalender = function(calendar) {
+    /* Change View  
+    $scope.renderCalender = function(calendar) {
       calendar.fullCalendar('render');
     };
-     $scope.uiConfig = {
+    /* config object  
+    $scope.uiConfig = {
       calendar:{
         height: 450,
         editable: true,
@@ -158,8 +169,10 @@ function CalendarCtrl($scope) {
         $scope.changeTo = 'Hungarian';
       }
     };
-     $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
+    /* event sources array 
+    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
     $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
- }
+    */
+}
 //End calendar! Tak tau amenda js panjang2 ni. Edit kot.
 
